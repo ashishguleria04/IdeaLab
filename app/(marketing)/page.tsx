ace4755e2a032cba187e8da0064b9d85fd00e8df
ashishguleria04 <guleriaashish65@gmail.com>
2024-10-12 20:36:24 +0530

    Some more structuring

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,15 @@
+import { Heading } from "./_components/heading";
+
+
+export default function Home() {
+  return (
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center
+      md:justify-center text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+
+      </div>
+
+    </div>
+  );
+}
