38141a3b175159b1b94c3c4c124893e82e531a04
ashishguleria04 <guleriaashish65@gmail.com>
2024-10-13 08:57:30 +0530

    landing page partially made

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 export const Heading = () => {
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-                Your Ideas, Documents, & Plans. Unified. Welcome to 
+                Your Ideas💡, Documents 📚, & Plans 😉. Unified. Welcome to 
                 <span className="underline"> IdeaLab </span>
             </h1>
             <h3 className="text-base sm:text-xl md:text-2xl font-medium ">
-                IdeaLab is the connect workspace where <br />
+                IdeaLab is the connected workspace where <br />
                 better, faster work happens.
             </h3>
             <Button>
                 Enter IdeaLab
-
-
+                <ArrowRight className="h-4 w-4 ml-2"/>
             </Button>
         </div>
     )
---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "600"]
+});
+
+export const Logo = () => {
+    return(
+        <div className="md:flex items-center gap-x-2">
+            <Image
+                src="/file.png"
+                height="80"
+                width="80"
+                alt="Logo"
+            />
+            <p className={cn("font-semibold", font.className)}>
+                IdeaLab
+            </p>
+
+        </div>
+    )
+}
