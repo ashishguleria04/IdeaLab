4b8a8f2b6f98a2b742084232eadb12cea1c3a598
ashishguleria04 <guleriaashish65@gmail.com>
2024-10-18 11:47:11 +0530

    added auth

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,17 +4,57 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Spinner } from "@/components/spinner";
 
 export const Navbar = () => {
-const scrolled = useScrollTop();
+    const { isAuthenticated, isLoading } = useConvexAuth();
+    const scrolled = useScrollTop();
 
     return (
         <div className={cn(
-            "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+            "z-50 bg-background dark:bg-[#262525] fixed top-0 flex items-center w-full p-6",
             scrolled && "border-b shadow-md"
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                
+                {isLoading && (
+                    <Spinner /> 
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size="sm">
+                                Log in
+                            </Button>
+
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm">
+                                Get IdeaLab free
+                            </Button>
+
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">
+                                Enter IdeaLab
+                            </Link>
+
+                        </Button>
+                        <UserButton 
+                            afterSignOutUrl="/"
+                        />
+                    
+                    </>
+                )}
                 <ModeToggle />
             </div>
         </div>
