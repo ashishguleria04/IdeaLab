390c29e162dea27017ab0cb4a03e7a11270ee062
ashishguleria04 <guleriaashish65@gmail.com>
2024-10-20 14:26:28 +0530

    sidebad done partially

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
 
 
 const MainLayout = ({
@@ -15,7 +16,7 @@ const MainLayout = ({
 
     if (isLoading){
         return(
-            <div className="h-full flex etems-center justify-center">
+            <div className="h-full flex items-center justify-center">
                 <Spinner size="lg"/>
             </div>
         );
@@ -28,7 +29,8 @@ const MainLayout = ({
 
 
     return ( 
-        <div className="h-full flex dark:bg-[#1F1F1F]"> 
+        <div className="h-full flex dark:bg-[#1F1F1F]">
+            <Navigation />
             <main className="flex-1 h-full overflow-y-auto">
                 {children}
             </main>    
